e28c4a77dc7ddd6db4d6db26162e5199f3397d6c
Kemp Atkinson <kempatkinson@gmail.com>
2019-09-20 12:47:53 -0500

    swqitching train

--- logic.js
+++ logic.js
@@ -1,113 +1,106 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
+// Steps to complete:
+
+// 1. Initialize Firebase
+// 2. Create button for adding new employees - then update the html + update the database
+// 3. Create a way to retrieve employees from the employee database.
+// 4. Create a way to calculate the months worked. Using difference between start and current time.
+//    Then use moment.js formatting to set difference in months.
+// 5. Calculate Total billed
+
+// 1. Initialize Firebase
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
     apiKey: "AIzaSyCxjNYwXcDGfKKuSeYtKjF6zvRayWYUJdQ",
     authDomain: "my-awesome-project-9a403.firebaseapp.com",
     databaseURL: "https://my-awesome-project-9a403.firebaseio.com",
     projectId: "my-awesome-project-9a403",
     storageBucket: "",
     messagingSenderId: "888538717903",
-    appId: "1:888538717903:web:509804196bd139824fe1ba"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
+    appId: "1:888538717903:web:81be1c46c70d214e4fe1ba"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  var database = firebase.database();
+  
+  // 2. Button for adding Employees
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainFrequency = $("#frequency-input").val().trim();
+    var trainStart = moment($("#start-input").val().trim(), "MM/DD/YYYY").format("X");
+  
+    // Creates local "temporary" object for holding employee data
+    var newTrain = {
+      name: trainName,
+      destination: trainDestination,
+      start: trainStart,
+      frequency: trainFrequency
+    };
+  
+    // Uploads employee data to the database
+    database.ref().push(newTrain);
+  
+    // Logs everything to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.frequency);
+    console.log(newTrain.start);
+  
+    alert("Train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#frequency-input").val("");
+    $("#start-input").val("");
+  });
+  
+  // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var trainStart = childSnapshot.val().start;
+    var trainFrequency = childSnapshot.val().frequency;
+  
+    // Employee Info
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(trainStart);
+    console.log(trainFrequency);
+  
+    // Prettify the employee start
+    
+    var trainStartpretty = moment.unix(trainStart).format("MM/DD/YYYY");
+  
+    // Calculate the next train arrival
 
-// Add ourselves to presence list when online.
-
-var connectionsRef = database.ref("/connections");
-
-// '.info/connected' is a special location provided by Firebase that is updated every time
-// the client's connection state changes.
-
-var connectedRef = database.ref(".info/connected");
-
-var connected = 0;
-// When the client's connection state changes...
-connectedRef.on("value", function (snap) {
-
-    // If they are connected..
-    if (snap.val()) {
-
-        if (connected < 2) {
-
-            // Add user to the connections list.
-            var con = connectionsRef.push(true);
-            connected++;
-
-            // Remove user from the connection list when they disconnect.
-            con.onDisconnect().remove();
-
-            //no way this can be right. how do i add when they connect, and remove when they disconnect?
-            con.onDisconnect(connected--);
-
-        }
-    }
-});
-
-
-$("#r1").on("click", function (event) {
-    if (snapshot.val().player1 != "") {
-        database.ref("/choices").set({
-            player1: "r"
-        })
-    }
-})
-// $("#p1").on("click", function (event) {
-//     if (snapshot.player1 != "") {
-//         database.ref("/choices").set({
-//             player1: "p"
-//         })
-//     }
-// })
-// $("#s1").on("click", function (event) {
-//     if (snapshot.player1 != "") {
-//         database.ref("/choices").set({
-//             player1: "s"
-//         })
-//     }
-// })
-
-
-// $("#r2").on("click", function (event) {
-//     if (snapshot.player2 != "") {
-//         database.ref("/choices").set({
-//             player2: "r"
-//         })
-//     }
-// })
-// $("#p2").on("click", function (event) {
-//     if (snapshot.player2 != "") {
-//         database.ref("/choices").set({
-//             player2: "p"
-//         })
-//     }
-// })
-$("#s2").on("click", function (event) {
-    if (snapshot.val().player2 != "") {
-        database.ref("/choices").set({
-            player2: "s"
-        })
-    }
-})
-
-var wins = 0;
-var ties = 0;
-var losses = 0;
-
-database.ref("choices").on("value", function(snapshot) {
-    console.log(snapshot);
-    if ((player1choice === "r") || (player1choice === "p") || (player1choice === "s") || (player2choice != "")) {
-
-        if ((player1choice === "r" && player2choice === "s") ||
-            (player1choice === "s" && player2choice === "p") ||
-            (player1choice === "p" && player2choice === "r")) {
-            wins++;
-            
-        } else if (player1choice === player2choice) {
-            ties++;
-        } else {
-            losses++;
-        }
-    }
-})
-
+    var trainArrival = 123;//////////;
+    
+  
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDestination),
+      $("<td>").text(trainFrequency),
+      $("<td>").text(trainArrival),
+    );
+  
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+  });
+  
+  // Example Time Math
+  // -----------------------------------------------------------------------------
+  // Assume Employee start date of January 1, 2015
+  // Assume current date is March 1, 2016
+  
+  // We know that this is 15 months.
+  // Now we will create code in moment.js to confirm that any attempt we use meets this test case
+  
